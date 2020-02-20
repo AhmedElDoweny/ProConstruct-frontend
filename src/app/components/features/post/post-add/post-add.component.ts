95da52bd95e51221a4adbebdf6b7b8fbda77a34b
EbrahimMansour2020 <ebrahimmansour8@gmail.com>
2020-02-20 14:03:28 +0200

    hhh

--- src/app/components/features/post/post-add/post-add.component.ts
+++ src/app/components/features/post/post-add/post-add.component.ts
@@ -13,29 +13,76 @@ export class PostAddComponent implements OnInit {
   
   // posts:Post[]=[];
   addPostForm:FormGroup;
+  imgpreview :string;
+  
+
+  get img(){
+    return this.addPostForm.get('image');
+  }
+
+  uploadFile(event){
+    const file = (event.target as HTMLInputElement).files[0];
+    this.addPostForm.patchValue({
+      image: file
+    });
+    this.addPostForm.get("image").updateValueAndValidity();
+
+    const reader = new FileReader();
+    reader.onload = ()=>{
+      this.imgpreview = reader.result as string;        
+    }
+    reader.readAsDataURL(file);
+}
+
+  // addPostt(){
+  //   //console.log(this.addPostForm.value);
+  //   this.postServ.addPost(this.addPostForm.value).subscribe(a=>{
+  //     console.log(a);
+  //   })  
+  // }
 
   addPostt(){
-    //console.log(this.addPostForm.value);
-    this.postServ.addPost(this.addPostForm.value).subscribe(a=>{
-      console.log(a);
-    })
+    this.postServ.addpost(
+                          this.addPostForm.value.title,
+                          this.addPostForm.value.category,
+                          this.addPostForm.value.description,
+                          this.addPostForm.value.price,
+                          this.addPostForm.value.image,
+                          this.addPostForm.value.client).subscribe(a=>{
+                            console.log(a);
+                            this.router.navigate(['/posts']);
+                          },error =>{
+                            console.log(error);
+                          })
+                          console.log(this.addPostForm.value);
+
+                              
   }
+
   constructor(private postServ:PostService,private router:Router) { }
 
-  ngOnInit() {
-    // this.postServ.getAllPosts().subscribe(a=>{
-    // this.posts=a;
-    // });
+  ngOnInit() {    
+    // this.addPostForm = new FormGroup({
+    //   // _id: new FormControl(100,Validators.required),
+    //   title: new FormControl("",Validators.required),
+    //   category:new FormControl("",Validators.required),
+    //   description:new FormControl("",Validators.required),
+    //   price:new FormControl(1000,Validators.required),
+    //   image:new FormControl("1.jpg",Validators.required),
+    //   client:new FormControl(1,Validators.required)
+    // })
+
     this.addPostForm = new FormGroup({
       // _id: new FormControl(100,Validators.required),
       title: new FormControl("",Validators.required),
       category:new FormControl("",Validators.required),
       description:new FormControl("",Validators.required),
-      price:new FormControl(1000,Validators.required),
-      image:new FormControl("1.jpg",Validators.required),
-      client:new FormControl(1,Validators.required)
+      price:new FormControl("1000",Validators.required),
+      image:new FormControl(""),
+      client:new FormControl("1",Validators.required)
     })
-  }
 
+  }
+  
 }
 

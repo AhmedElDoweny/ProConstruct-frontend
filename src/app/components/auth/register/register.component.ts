7cd3f692acbf478051b1b568bdb3273d6b1622c9
Moaz120 <moaz120560@gmail.com>
2020-02-24 10:02:14 +0200

    add validation and fix posts in middle header

--- src/app/components/auth/register/register.component.ts
+++ src/app/components/auth/register/register.component.ts
@@ -2,6 +2,7 @@ import {Router} from '@angular/router';
 import {Component, OnInit} from '@angular/core';
 import {ClientService} from '../../../_service/client.service';
 import {FormGroup, FormControl, Validators} from '@angular/forms';
+import {NgFlashMessageService} from 'ng-flash-messages'
 
 @Component({
   selector: 'app-register',
@@ -11,6 +12,23 @@ import {FormGroup, FormControl, Validators} from '@angular/forms';
 export class RegisterComponent implements OnInit {
   register: FormGroup;
   emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  get Name(){
+    return this.register.get('name')
+  }
+  get Email(){
+    return this.register.get('email')
+  }
+  get Password(){
+    return this.register.get('password')
+  }
+  get Phone(){
+    return this.register.get('phone')
+  }
+  get Role(){
+    return this.register.get('role')
+  }
+
+
 
   onSave() {
     console.log(this.register.value);
@@ -18,27 +36,39 @@ export class RegisterComponent implements OnInit {
       data => {
         this.clientSer.setToken(data['token']);
         this.router.navigateByUrl('/profile');
+        this.clientSer.changeFlag(true)
       },
       err => {
-        console.log(err.error.message);
+        if(err.error.length == 1){
+          this.NgFlashMessageService.showFlashMessage({
+            messages:[`${err.error[0]}`],
+            type : "danger"
+          })
+        }
+        else { 
+        this.NgFlashMessageService.showFlashMessage({
+          messages:[`Please enter the required fields`],
+          type : "danger"
+        })}
       }
     );
-    this.clientSer.changeFlag(true)
-
   }
 
-  constructor(private clientSer: ClientService, private router: Router) {
-  }
+  constructor(
+    private clientSer: ClientService,
+    private router: Router,
+    private NgFlashMessageService: NgFlashMessageService)
+    {}
 
   ngOnInit() {
 
     this.register = new FormGroup({
-      //"_id": new FormControl(9,Validators.required),
-      'name': new FormControl('', Validators.required),
-      'password': new FormControl('', Validators.required),
+      //"_id": new FormControl(9,Validators.required),  ^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$
+      'name': new FormControl('', [Validators.required,Validators.pattern(`^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$`)]),
+      'password': new FormControl('', [Validators.required, Validators.pattern(`^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}$`)]),
       'email': new FormControl('', [Validators.required, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$')]),
-      'phone': new FormControl('', Validators.required),
-      'role': new FormControl('Engineer', Validators.required)
+      'phone': new FormControl('', [Validators.required, Validators.pattern(`^[0][1][012][0-9]{8}$`)]),
+      'role': new FormControl('', Validators.required)
     });
     if (this.clientSer.isLoggedIn()) {
       this.router.navigateByUrl('/profile');

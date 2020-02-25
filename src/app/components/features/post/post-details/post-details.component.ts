8c7d811044d1fbee3a71cb5c864332e39d42f91f
mohamedmadian10 <mohamed.madian183@gmail.com>
2020-02-25 17:57:40 +0200

    remove order button

--- src/app/components/features/post/post-details/post-details.component.ts
+++ src/app/components/features/post/post-details/post-details.component.ts
@@ -15,21 +15,25 @@ import { CartServiceService } from 'src/app/_service/cart.service';
 export class PostDetailsComponent implements OnInit {
  ordered:boolean=true;
  loged=false;
+ clientid; 
   constructor(private postServ: PostService, private aroute: ActivatedRoute,private cartservice:CartServiceService,private clientservice:ClientService) {
   }
 
   postInfo: Post = new Post(1, '', '', '', 1000, '', new Client());
-
+  
   ngOnInit() {
     this.aroute.params.subscribe(a => {
       this.postServ.getPostDetails(a.id).subscribe(s => {
         this.postInfo = s;
-        console.log(s);
+        console.log("post det",s);
+        console.log("cid",this.clientid)
 
       });
       
     });
     this.loged= this.clientservice.isLoggedIn();
+    this.clientid = this.clientservice.getUserPayload()._id;
+    
   }
 
 }

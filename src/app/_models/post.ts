6bb6734ba415a56d601d5884492f1d8a674f5a81
AhmedElDoweny <ahmed.ali.eldoweny@gmail.com>
2020-03-10 16:19:50 +0200

    add some edits

--- src/app/_models/post.ts
+++ src/app/_models/post.ts
@@ -2,13 +2,13 @@ import {Client} from './client';
 
 export class Post {
   constructor(
-    public _id: number,
-    public title: string,
-    public category: string,
-    public description: string,
-    public price: number,
-    public image: string,
-    public client: Client,
+    public _id?: number,
+    public title?: string,
+    public category?: string,
+    public description?: string,
+    public price?: number,
+    public image?: string,
+    public client?: Client,
     public location?: { lng: number, lat: number }
   ) {
   }
